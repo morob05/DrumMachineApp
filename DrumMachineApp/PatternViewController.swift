269bdd2568b37c0e4ed11a2d9414bfc82ccc2182
Morten Robinson <mortenrobinson@hotmail.com>
2018-11-23 06:11:12 +0100

    Started on Pattern view... still needs some work, needs to be scrollable.

---
+++ DrumMachineApp/PatternViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PatternViewController.swift
+//  DrumMachineApp
+//
+//  Created by Morten Robinson on 23/11/2018.
+//  Copyright © 2018 Morten Robinson. All rights reserved.
+//
+
+import UIKit
+
+class PatternViewController: UIViewController {
+    
+}
