d1f71b87816a21e70f004bffbaeff4913864c1b5
Morten Robinson <mortenrobinson@hotmail.com>
2018-11-23 05:19:34 +0100

    Pad view

---
+++ DrumMachineApp/PadViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  DrumMachineApp
+//
+//  Created by Morten Robinson on 22/11/2018.
+//  Copyright © 2018 Morten Robinson. All rights reserved.
+//
+
+import UIKit
+
+class PadViewController: UIViewController {
+    
+    @IBAction func padTap(_ sender: UIButton) {
+        print("Tapped a pad!")
+        //TODO use sender index to trigger sound
+    }
+}
+
